5cd3eabb446448a0b11e1b4b6fca293b484f9211
itcast <hello@itcast.cn>
2025-05-23 19:22:11 +0800

    project init

---
+++ ourchat-client/src/main/java/cn/langoo/ourchat/OurchatClientApplication.java
@@ -0,0 +1,13 @@
+package cn.langoo.ourchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurchatClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurchatClientApplication.class, args);
+    }
+
+}
---
+++ ourchat-client/src/test/java/cn/langoo/ourchat/OurchatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.langoo.ourchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurchatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
